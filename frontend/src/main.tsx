886548fa01620c036704ecd89d59d6574bf32dc2
AmanPrakash09 <amanprakashburnett@gmail.com>
2026-04-29 13:17:18 -0700

    Deleted unneeded ModeProvider.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,7 @@
-//import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ModeProvider } from './providers/ModeProvider'
 
 createRoot(document.getElementById('root')!).render(
-  //<StrictMode>
-    <ModeProvider>
-      <App />
-    </ModeProvider>
-  //</StrictMode>,
+    <App />
 )
